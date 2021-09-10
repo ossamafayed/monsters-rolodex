f6103fc5fd0ffb3e6e03a4e481fbd5e5c6e32fcf
osama fayed <osamaifayed@gmail.com>
2021-09-10 10:52:31 +0400

    Updates

---
+++ precache-manifest.07f5e6d98ada6052af593d2f4e7b6964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb7bb473ae13a6307213df4b7414eaf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58a150a98924f631c052",
+    "url": "/monsters-rolodex/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a150a98924f631c052",
+    "url": "/monsters-rolodex/static/js/main.d4212be8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
